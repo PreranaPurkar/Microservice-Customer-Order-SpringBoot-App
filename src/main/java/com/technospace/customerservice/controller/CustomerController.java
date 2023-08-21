f4b2c54b1fce73657396941230eb73ec6e382f5d
PreranaPurkar <preranapurkar12@gmail.com>
2023-08-21 15:53:47 +0530

    [ADDED]:[Prerana]RestTemplate Application is used for get customer by cid

--- src/main/java/com/technospace/customerservice/controller/CustomerController.java
+++ src/main/java/com/technospace/customerservice/controller/CustomerController.java
@@ -1,9 +1,11 @@
 package com.technospace.customerservice.controller;
 
 import com.technospace.customerservice.entity.CustomerE;
+import com.technospace.customerservice.entity.OrdersE;
 import com.technospace.customerservice.repo.ICustomer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -13,6 +15,8 @@ public class CustomerController {
     @Autowired
     ICustomer iCustomer;
 
+    @Autowired
+    RestTemplate restTemplate;
     @PostMapping("/addcustomer")
     public CustomerE addcustomer(@RequestBody CustomerE customerE){
         return iCustomer.save(customerE);
@@ -21,5 +25,13 @@ public class CustomerController {
     @GetMapping("/getallcustomers")
     public List<CustomerE> getallcustomers(){
         return iCustomer.findAll();
+    }
+
+    @GetMapping("/getcustomerbycid/{cid}")
+    public CustomerE getcustomerbycid(@PathVariable("cid") int cid){
+        CustomerE customerE = iCustomer.findById(cid).get();
+        List<OrdersE> ordersEList = restTemplate.getForObject("http://localhost:8003/order/api/getordersbycid/"+cid,List.class);
+        customerE.setOrdersEList(ordersEList);
+        return customerE;
     }
 }
